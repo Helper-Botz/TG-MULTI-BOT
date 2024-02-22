52c7d72fd53f609de7e9f838dc80746fca88ab64
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-23 00:50:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
-apt update && apt upgrade -y
-apt install ffmpeg -y
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Helper-Botz/TG-MULTI-BOT.git /TG-MULTI-BOT
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /TG-MULTI-BOT
+fi
+cd /TG-MULTI-BOT
 pip3 install -U -r requirements.txt
-echo "Starting the Bot...."
-python3 loader.py
+echo "Starting Bot...."
+python3 lilsa.py
