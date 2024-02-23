addcb7a24af7d36c746c848a8b33c897fd815e63
HelperCode <109080360+Helper-Botz@users.noreply.github.com>
2024-02-23 09:40:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Helper-Botz/TG-MULTI-BOT.git /TG-MULTI-BOT
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG-MULTI-BOT
-fi
 apt update && apt upgrade -y
 apt install ffmpeg -y
 pip3 install -U -r requirements.txt
